7f429987b1570ed7fa73fece6e1962720f3cf82f
Anastasia Belousova <belousova_anastasia@yahoo.com>
2021-05-25 19:24:36 -0400

    1st Commit

---
+++ src/test/java/BranchingPractice.java
@@ -0,0 +1,10 @@
+public class BranchingPractice {
+    public static void main(String[] args) {
+
+        /**
+         * Created new project
+         * Enabled version control
+         * Added .gitignore
+         */
+    }
+}
